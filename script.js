781365b268ed620d18e41bd28f477c5a48e6adb8
Laíse Vieira <laisevdev@gmail.com>
2023-01-19 15:44:03 -0300

    ativa links menu

---
+++ script.js
@@ -0,0 +1,13 @@
+const linksCabecalho = document.querySelectorAll ('.menu-cabecalho li');
+
+function atvLinkCabecalho (event) {
+    event.preventDefault();
+    linksCabecalho.forEach ((links) => {
+       links.removeAttribute ('class', 'LINK-ATIVO');
+    })
+    event.currentTarget.setAttribute ('class', 'LINK-ATIVO');
+};
+
+linksCabecalho.forEach ((links) => {
+    links.addEventListener ('click', atvLinkCabecalho);
+});
