871ad0fa4a8f74c98ccc1bfe68f9541be8575b1f
Laíse Vieira <laisevdev@gmail.com>
2023-01-24 22:33:55 -0300

    refactor: isola funcao ativaLinks

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const linksCabecalho = document.querySelectorAll ('.menu-cabecalho li');
+function ativaLinks () {
+    const linksCabecalho = document.querySelectorAll ('.menu-cabecalho li');
 
-function atvLinkCabecalho (event) {
-    event.preventDefault();
+    function atvLinkCabecalho (event) {
+        event.preventDefault();
+        linksCabecalho.forEach ((links) => {
+        links.removeAttribute ('class', 'LINK-ATIVO');
+        })
+        event.currentTarget.setAttribute ('class', 'LINK-ATIVO');
+    };
+
     linksCabecalho.forEach ((links) => {
-       links.removeAttribute ('class', 'LINK-ATIVO');
-    })
-    event.currentTarget.setAttribute ('class', 'LINK-ATIVO');
-};
+        links.addEventListener ('click', atvLinkCabecalho);
+    });
+}
 
-linksCabecalho.forEach ((links) => {
-    links.addEventListener ('click', atvLinkCabecalho);
-});
+ativaLinks();
+
+const 
